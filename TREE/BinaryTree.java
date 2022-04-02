d7529b7f1ed0692b86dada0832726aac6c5a5583
Ankur357 <ankur.gautam_cs20@gla.ac.in>
2022-04-02 10:33:35 +0530

    tree

--- TREE/BinaryTree.java
+++ TREE/BinaryTree.java
@@ -10,15 +10,19 @@ class Node{
 }
 public class BinaryTree {
     Node root;
-    // BinaryTree(int key){
-    //     root = new Node(key);
-    // }
+    public void traverseTree(Node node) {
+        if (node != null) {
+          traverseTree(node.left);
+          System.out.print(" " + node.data);
+          traverseTree(node.right);
+        }
+        }
     public static void main(String[] args) {
         BinaryTree tree = new BinaryTree();
         tree.root = new Node(1);
         tree.root.left = new Node(2);
         tree.root.right = new Node(3);
-
+        tree.traverseTree(tree.root);
     }
     
 }
