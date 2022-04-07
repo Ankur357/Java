155c75049cf5a8f673e3041be5494b6c14d18cb7
Ankur357 <93982657+Ankur357@users.noreply.github.com>
2022-04-07 11:12:24 +0530

    Create deleteNodeBST.java

---
+++ TREE/deleteNodeBST.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int max(TreeNode node){
+        TreeNode curr = node;
+        while(curr.right!=null){
+            curr = curr.right;
+        }
+        return curr.val;
+    }
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root==null){
+            return null;
+        }
+        if(key>root.val){
+            root.right = deleteNode(root.right,key);
+        }
+        else if(key<root.val){
+            root.left = deleteNode(root.left,key);
+        }
+        else{
+            if(root.right==null || root.left ==null){
+                return root.left != null ? root.left : root.right;
+            }
+            
+            int maxele = max(root.left);
+            root.val = maxele;
+            root.left = deleteNode(root.left, maxele);
+        }
+        return root;
+    }
+}
